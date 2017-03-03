c6d7a6a7528c57abcdb495b78e4ecf1c6dc4f866
Peter Kirchev <petyr.kirchev@gmail.com>
2017-03-03 17:47:26 +0200

    ОБАДЕТЕ НИ СААА

---
+++ assets/js/callUs.js
@@ -0,0 +1,15 @@
+function callUs() {
+    var element1 = document.getElementById("hoverSpan");
+    element1.style.visibility = "visible";
+    var element2 = document.getElementById("hoverSpanAfter");
+    element2.style.visibility = "visible";
+
+
+}
+
+function callUsRemove() {
+    var element1 = document.getElementById("hoverSpan");
+    element1.style.visibility = "hidden";
+    var element2 = document.getElementById("hoverSpanAfter");
+    element2.style.visibility = "hidden";
+}
