9e9dcc970c9a322f2805cba54c02c798a9ed4759
Peter Kirchev <petyr.kirchev@gmail.com>
2017-03-17 22:29:11 +0200

    1x1

--- assets/js/loginService.js
+++ assets/js/loginService.js
@@ -1,3 +1,39 @@
-function User(name,password){
-
+function User(name,surname,email,password){
+    this.name=name;
+    this.surname=name;
+    this.email=email;
+    this.password=password;
 }
+
+var userManager = (function(){
+    var users=[new User("Peter","Kirchev","petrakis1987@abv.bg","president")];
+
+    return{
+        addUser:function(user){
+            if(user instanceof User){
+                users.push(user);
+            }
+        },
+
+        loginUser:function(email,password){
+            return users.some(function(u){
+                return (u.email === email) &&
+                    (u.password === password);
+            })
+        },
+
+        isUserExists: function (email){
+            return users.some(function(u){
+                return (u.email === email);
+            })
+        },
+
+        getNumberOfUsers: function(){
+            return users.length;
+        },
+
+        isPasswordValid: function(password){
+            return password.length > 5;
+        }
+    }
+})();
