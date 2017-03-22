f3c2fa881bc04c14f61220729123a53fa1add48b
Peter Kirchev <petyr.kirchev@gmail.com>
2017-03-22 11:40:54 +0200

    2nd try FB log

---
+++ fbLogApi.js
@@ -0,0 +1,27 @@
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '187620591743427',
+            xfbml: true,
+            version: 'v2.8'
+        });
+        FB.AppEvents.logPageView();
+
+    };
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    FB.login(function (response) {
+        if (response.status === 'connected') {
+            console.log("connected");
+        } else {
+            console.log("not connected");
+        }
+    });
